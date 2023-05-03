3c78a7540180e8cc787ff2bf12269689cffc41b2
inerer <arshinov04@gmail.com>
2023-05-03 10:40:07 +0300

    settingscard ui

---
+++ Cookbook/UI/SettingsCard.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Cookbook.UI;
+
+[ContentProperty("Content")]
+public partial class SettingsCard : UserControl
+{
+    public SettingsCard()
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+
+    #region InnerContent
+
+    public new FrameworkElement Content
+    {
+        get => (FrameworkElement)GetValue(ContentProperty);
+        set
+        {
+            SetValue(ContentProperty, value);
+            ContentControl.Content = value;
+        }
+    }
+
+    // Using a DependencyProperty as the backing store for InnerContent.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty ContentProperty =
+        DependencyProperty.Register(
+            nameof(Content),
+            typeof(FrameworkElement),
+            typeof(SettingsCard),
+            new UIPropertyMetadata(null));
+
+    #endregion
+
+    public static readonly DependencyProperty HeaderProperty =
+        DependencyProperty.Register(
+            nameof(Header),
+            typeof(string),
+            typeof(SettingsCard));
+
+    public static readonly DependencyProperty DescriptionProperty =
+        DependencyProperty.Register(
+            nameof(Description),
+            typeof(string),
+            typeof(SettingsCard));
+
+    public static readonly DependencyProperty ImageSourceProperty =
+        DependencyProperty.Register(
+            nameof(ImageSource),
+            typeof(ImageSource),
+            typeof(SettingsCard));
+
+
+    public string Header
+    {
+        get => (string)GetValue(HeaderProperty);
+        set
+        {
+            SetValue(HeaderProperty, value);
+            HeaderTextBlock.Text = HeaderProperty.ToString();
+        }
+    }
+
+    public string Description
+    {
+        get => (string)GetValue(DescriptionProperty);
+        set
+        {
+            SetValue(HeaderProperty, value);
+            DescriptionTextBlock.Text = DescriptionProperty.ToString();
+        }
+    }
+
+    public ImageSource ImageSource
+    {
+        get => (ImageSource)GetValue(ImageSourceProperty);
+        set
+        {
+            SetValue(ImageSourceProperty, value);
+            Icon.Source = value;
+        }
+    }
+}
