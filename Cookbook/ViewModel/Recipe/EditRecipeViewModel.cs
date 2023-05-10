9dad9b53abe1e7e68374f0ad7f8172f6c5c92900
golybock <teplovartem3094@gmail.com>
2023-05-10 20:58:44 +0300

    start refactoring

--- Cookbook/ViewModel/Recipe/EditRecipeViewModel.cs
+++ Cookbook/ViewModel/Recipe/EditRecipeViewModel.cs
@@ -1,8 +1,12 @@
-﻿using Cookbook.Command;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using Cookbook.Api.Recipe;
+using Cookbook.Command;
 using Cookbook.Models.Blank.Recipe;
 using Cookbook.Models.Blank.Recipe.Category;
 using Cookbook.Models.Blank.Recipe.Ingredient;
 using Cookbook.Models.Domain.Recipe;
+using Cookbook.Services;
 using Cookbook.ViewModel.Navigation;
 using ModernWpf.Controls;
 
@@ -10,20 +14,54 @@ namespace Cookbook.ViewModel.Recipe;
 
 public class EditRecipeViewModel : ViewModelBase, INavItem
 {
+    private ObservableCollection<RecipeTypeDomain> _recipeTypeDomains = new ObservableCollection<RecipeTypeDomain>();
+    private RecipeService _recipeService = new RecipeService();
+    private RecipeTypeDomain _selectedRecipeTypeDomain = new RecipeTypeDomain();
     public RecipeBlank Recipe { get; set; }
 
+    public string? ImageUrl { get; set; }
+
     public INavHost Host { get; set; }
+
+    public ObservableCollection<RecipeTypeDomain> RecipeTypeDomains
+    {
+        get => _recipeTypeDomains;
+        set
+        {
+            if (Equals(value, _recipeTypeDomains)) return;
+            _recipeTypeDomains = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public RecipeTypeDomain SelectedRecipeTypeDomain
+    {
+        get => _selectedRecipeTypeDomain;
+        set
+        {
+            if (Equals(value, _selectedRecipeTypeDomain)) return;
+            _selectedRecipeTypeDomain = value;
+
+            Recipe.TypeId = value.Id;
+            
+            OnPropertyChanged();
+        }
+    }
 
     public EditRecipeViewModel(INavHost host)
     {
         Host = host;
         Recipe = new RecipeBlank();
+        GetRecipeTypes();
     }
 
     public EditRecipeViewModel(INavHost host, RecipeDomain recipeDomain)
     {
         Host = host;
         Recipe = new RecipeBlank(recipeDomain);
+        GetRecipeTypes();
+
+        ImageUrl = recipeDomain.ImagePath;
     }
 
     public CommandHandler CancelCommand =>
@@ -50,6 +88,24 @@ public class EditRecipeViewModel : ViewModelBase, INavItem
     public CommandHandler<RecipeIngredientBlank> DeleteIngredientCommand =>
         new CommandHandler<RecipeIngredientBlank>(DeleteIngredient);
 
+    public async void GetRecipeTypes()
+    {
+        var types = await _recipeService.RecipeType.Get();
+
+        if (types != null) 
+            RecipeTypeDomains = new(types);
+        
+        RecipeTypeDomains.Add(new RecipeTypeDomain()
+        {
+            Id = 0, 
+            Name = "Не выбрано" 
+        });
+
+        SelectedRecipeTypeDomain = RecipeTypeDomains.LastOrDefault()!;
+        
+        OnPropertyChanged("RecipeTypeDomains");
+    }
+    
     private async void CancelEdit()
     {
         var dialog = new ContentDialog()
